4aa4cd59fce758abe65b77f4c1747c54afecec30
leeseunghee00 <lsh9246@gmail.com>
2025-04-11 17:07:00 +0900

    initialize

---
+++ src/main/java/io/leeseunghee/jpajdbcpagingtest/JpaJdbcPagingTestApplication.java
@@ -0,0 +1,13 @@
+package io.leeseunghee.jpajdbcpagingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaJdbcPagingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaJdbcPagingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/leeseunghee/jpajdbcpagingtest/JpaJdbcPagingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.leeseunghee.jpajdbcpagingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaJdbcPagingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
